5913f78d758d0bc36d896e3397fe7d44f89c68aa
MiklePvo <mishmail@ukr.net>
2016-11-08 21:05:06 +0200

    Create base classes

---
+++ src/main/java/com/gmail/config/Config.java
@@ -0,0 +1,9 @@
+package com.gmail.config;
+
+/**
+ * Created by Mikle on 08.11.2016.
+ */
+public class Config {
+    public static String emailAdress = "fortestingjohntester@gmail.com";
+    public static String password = "gmailtest";
+}
---
+++ src/main/java/com/gmail/pages/GmailMainPage.java
@@ -0,0 +1,7 @@
+package com.gmail.pages;
+
+/**
+ * Created by Mikle on 08.11.2016.
+ */
+public class GmailMainPage {
+}
