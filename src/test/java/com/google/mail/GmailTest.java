a95175b6bad919e1eeb8790f3f4fefd421d1c0a6
MiklePvo <mishmail@ukr.net>
2017-01-04 21:44:10 +0200

    Delete BaseTest

--- src/test/java/com/google/mail/GmailTest.java
+++ src/test/java/com/google/mail/GmailTest.java
@@ -1,19 +1,24 @@
 package com.google.mail;
 
 import com.google.mail.pages.GmailPage;
+import org.junit.AfterClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Calendar;
 
 import static com.google.mail.core.ConciseAPI.assertThat;
+import static com.google.mail.core.CustomConditions.nthProxyElementHasText;
 import static com.google.mail.core.CustomConditions.texts;
 import static com.google.mail.testdata.Config.emailAddress;
 import static com.google.mail.testdata.Config.password;
-import static com.google.mail.core.CustomConditions.nthProxyElementHasText;
 
-public class GmailTest extends BaseTest {
+public class GmailTest {
 
     private GmailPage page = new GmailPage(driver);
+
+    private static WebDriver driver = new FirefoxDriver();
 
     @Test
     public void testLoginSendReceiveSearch() {
@@ -32,5 +37,10 @@ public class GmailTest extends BaseTest {
         page.clickInbox();
         page.search(subject);
         assertThat(driver, texts(page.emails, subject));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 }
