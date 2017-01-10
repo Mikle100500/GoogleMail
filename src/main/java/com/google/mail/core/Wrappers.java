225f8f23215e4d54bc97c5c893ff2383762d0ea7
MiklePvo <mishmail@ukr.net>
2017-01-10 20:42:16 +0200

    Redo catcher

--- src/main/java/com/google/mail/core/Wrappers.java
+++ src/main/java/com/google/mail/core/Wrappers.java
@@ -1,29 +1,23 @@
 package com.google.mail.core;
 
-import org.openqa.selenium.ElementNotVisibleException;
+import com.google.common.base.Function;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
-import com.google.common.base.Function;
-
 public class Wrappers {
-    public static <V> ExpectedCondition<V> elementExceptionsCatcher(final Function<? super WebDriver, V> condition){
+    public static <V> ExpectedCondition<V> elementExceptionsCatcher(final Function<? super WebDriver, V> condition) {
         return new ExpectedCondition<V>() {
-            
+
             public V apply(WebDriver input) {
                 try {
                     return condition.apply(input);
-                } catch (StaleElementReferenceException e) {
-                    return null;
-                } catch (ElementNotVisibleException e){
-                    return null;
-                } catch (IndexOutOfBoundsException e){
+                } catch (StaleElementReferenceException | IndexOutOfBoundsException e) {
                     return null;
                 }
             }
 
-            public String toString(){
+            public String toString() {
                 return condition.toString();
             }
         };
